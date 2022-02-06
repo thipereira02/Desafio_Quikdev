9e38928927c9376dbb2dfad1d2eddee1827c414d
thipereira02 <thiago.spereira02@gmail.com>
2022-02-06 18:36:13 -0300

    Refactoring auth service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,13 +1,13 @@
 import LogInData from '../interfaces/logIn';
 import User from '../entities/User';
 import Session from '../entities/Session';
-import { logInSchema } from '../schemas/logInSchema';
+import { validateLogInBody } from '../utils/validateBody';
 
 export async function signIn(userData: LogInData) {
   const { email, password } = userData;
 
-  const bodyIsValid = logInSchema.validate(userData);
-  if (bodyIsValid.error !== undefined) return undefined;
+  const validate = validateLogInBody(userData);
+  if (!validate) return undefined;
 
   const user = await User.findByEmailAndPassword(email, password);
   if (!user) return false;
--- src/utils/validateBody.ts
+++ src/utils/validateBody.ts
@@ -1,10 +1,18 @@
 import SignUpData from '../interfaces/signUp';
+import LogInData from '../interfaces/logIn';
 import UpdateData from '../interfaces/update';
 import { signUpSchema } from '../schemas/signUpSchema';
+import { logInSchema } from '../schemas/logInSchema';
 import { updateSchema } from '../schemas/updateSchema';
 
 export function validateSignUpBody(userData: SignUpData) {
   const bodyIsValid = signUpSchema.validate(userData);
+  if (bodyIsValid.error !== undefined) return undefined;
+  return true;
+}
+
+export function validateLogInBody(userData: LogInData) {
+  const bodyIsValid = logInSchema.validate(userData);
   if (bodyIsValid.error !== undefined) return undefined;
   return true;
 }
